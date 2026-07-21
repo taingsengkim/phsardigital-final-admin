601426db1e7ca7d3a5db83e4f2e0f7e0666ab73e
Sim Meng Hor <menghor.it@gmail.com>
2026-07-21 08:10:35 +0700

    config font inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import {TooltipProvider} from "@/components/ui/tooltip";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -22,18 +15,23 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
-    >
+      <html
+          lang="en"
+          className={cn(
+              "h-full",
+              "antialiased",
+              inter.variable,
+              "font-sans"
+          )}
+      >
       <body className="min-h-full flex flex-col">
       <TooltipProvider>{children}</TooltipProvider>
       </body>
-    </html>
+      </html>
   );
 }
